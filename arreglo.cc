02023c3322894406f41d7a52641d97176b8aa5a1
2231174797 <31254610+2231174797@users.noreply.github.com>
2017-10-31 13:19:56 -0600

    Add files via upload
    Modificado

--- arreglo.cc
+++ arreglo.cc
@@ -2,18 +2,21 @@
 using namespace std;
 int main()
 {
+  size_t talla=16;
+  cout<<"¿Cuantos alumnos encuestaras?"<<endl;
+  cin>>talla;
 int sum=0;
-int edad[16];
-for(int i=0;i<16;i++)
+int*edad=new int[talla];
+for(int i=0;i<talla;i++)
   {
     cout<<"Dame tu edad"<<endl;
     cin>>edad[i];
   }
-for(int j=0;j<16;j++)
+for(int j=0;j<talla;j++)
   sum+=(edad[j]+3);
-for(int j=0;j<16;j++)
+for(int j=0;j<talla;j++)
   cout<<edad[j]<<" , ";
 cout<<endl;
-cout<<"el promedio de edad a la que se graduara este grupo es_"<<sum/16<<endl;
+cout<<"el promedio de edad a la que se graduara este grupo es_"<<sum/talla<<endl;
 return 0;
 }
